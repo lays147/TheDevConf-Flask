ac24bf11c189d4223cbb9104726362faa93ab421
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-07-17 21:05:09 -0300

    Add 404 template and exception handler
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- landingpage/main.py
+++ landingpage/main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 #Init Flask
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
